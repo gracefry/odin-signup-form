6645e3f71c1280bc6c2c0cc5aa9a10cefe97cd11
Grace <99232016+gracefry@users.noreply.github.com>
2023-01-10 19:43:25 +1100

    JS password validation working

--- javascript.js
+++ javascript.js
@@ -1,13 +1,19 @@
-// const button = document.querySelector("#submit");
-// button.addEventListener("click", verifyPassword());
-
-function verifyPassword() {
-    let password = document.querySelector("#password").value;
-    let conf = document.querySelector("#confirm-password").value;
-    if(password != conf)  
+function verifyPassword(e) {
+    let password = document.querySelector("#password");
+    let conf = document.querySelector("#confirm-password");
+    if(password.value != conf.value)  
     {
-        alert("Passwords did not match");  
-    } else {  
-        alert("Password created successfully");  
+        e.preventDefault();
+        password.style.borderColor = "red";
+        const errorText = document.createElement("span");
+        const column = document.querySelector("#column1")
+        errorText.textContent = "* Passwords do not match";
+        errorText.style.cssText = "font-size: 0.7rem; color: red;";
+        column.appendChild(errorText);
+        conf.style.borderColor = "red"; 
     }  
 }
+
+const button = document.querySelector("#submit");
+button.addEventListener("click", verifyPassword);
+
